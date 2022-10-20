a0268063720a9a0ac41a3cdddf8a0a7a45b4ed50
taoufik chahouat <taoufik.chahouat@gmail.com>
2022-10-20 06:25:13 +0200

    add color states

--- src/components/FlagComponent.js
+++ src/components/FlagComponent.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function FlagComponent(props) {
     return (
-        <div className='div-flag-component' style={{position:'absolute', top:props.clientY-50+'px', left:props.clientX-50+'px'}}>
+        <div className='div-flag-component' style={{position:'absolute', top:props.clientY-70+'px', left:props.clientX-70+'px'}}>
             <div className='div-flag-container'>
                 <p>{props.name}</p> 
                 <img className='img-flag-component' src={props.image}></img>
             </div>
-            <i className="fas fa-solid fa-caret-down"></i>
+            {/* <i className="fas fa-solid fa-caret-down"></i> */}
         </div>
     );
 }
